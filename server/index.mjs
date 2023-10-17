4cc564417193b5e40103133598e4fcd9faa83cfe
Ben Ray <bray22@gmail.com>
2023-10-17 12:30:59 -0400

    remove node_module

---
+++ server/index.mjs
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import './loadEnvironment.mjs';
+import 'express-async-errors';
+import beers from './routes/beers.mjs';
+import breweries from './routes/breweries.mjs';
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/beers', beers);
+app.use('/breweries', breweries);
+
+// Global error handling
+app.use((err, _req, res, _next) => {
+  console.error(err); // Log the error for debugging
+  res.status(500).send('Uh oh! An unexpected error occurred.');
+});
+
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
