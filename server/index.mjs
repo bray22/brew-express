3af19ed3553b310da4e48cd77f8348f9b5ee6272
Ben Ray <bray22@gmail.com>
2023-10-17 22:16:49 -0400

    add cert

--- server/index.mjs
+++ server/index.mjs
@@ -5,8 +5,19 @@ import 'express-async-errors';
 import beers from './routes/beers.mjs';
 import breweries from './routes/breweries.mjs';
 
+const https_port = 443; // The default HTTPS port
 const PORT = process.env.PORT || 80;
 const app = express();
+const fs = require('fs');
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/www.raystar.io/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/www.raystar.io/fullchain.pem', 'utf8');
+
+const https = require('https');
+const credentials = { key: privateKey, cert: certificate };
+
+const httpsServer = https.createServer(credentials, app);
+
 
 // Middlewares
 app.use(cors());
@@ -30,5 +41,9 @@ app.listen(PORT, () => {
 // Catch-all route for undefined paths (all HTTP methods)
 app.all('*', (req, res) => {
   res.status(404).send('Oops! The page you requested does not exist.');
+});
+
+httpsServer.listen(https_port, () => {
+  console.log(`Server is running on https://www.raystar.io:${https_port}`);
 });
 
