ba51a28a73fd6fbfbb9dbfe67ba0b75b8cd81276
Ben Ray <bray22@gmail.com>
2023-10-31 23:31:44 -0400

    add code

--- server/routes/auth.mjs
+++ server/routes/auth.mjs
@@ -5,17 +5,25 @@ import passport from "passport";
 const router = express.Router();
 //const CLIENT_URL = "http://localhost:3000/"
 
-router.get("/login/success", (req,res) => {
-  if (req.user) {
-    res.status(200).json({
-      error: false,
-      message: "Successfully Logged In",
-      user: req.user
-    });
-    console.log("SUCCESS");
-  } else {
-    res.status(403).json({error: true, message: "Not Authorized"})
-  }
+// router.get("/login/success", (req,res) => {
+//   if (req.user) {
+//     res.status(200).json({
+//       error: false,
+//       message: "Successfully Logged In",
+//       user: req.user
+//     });
+//     console.log("SUCCESS");
+//   } else {
+//     res.status(403).json({error: true, message: "Not Authorized"})
+//   }
+// });
+
+router.get('/login/success', (req, res) => {
+  // Extract the code from the request object
+  const code = req.query.code;
+
+  // Redirect the user to the status.html page with the code
+  res.redirect(`/status.html?code=${code}`);
 });
 
 router.get("/login/failed", (req,res) => {
