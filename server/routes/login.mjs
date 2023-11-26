10d42e07bd85afe9186c47255d6ed9c74e16e496
Ben Ray <bray22@gmail.com>
2023-11-25 20:14:09 -0500

    login update

--- server/routes/login.mjs
+++ server/routes/login.mjs
@@ -7,20 +7,74 @@ import sgMail from '@sendgrid/mail'; // Change the import
 
 const router = express.Router();
 
+// new user
+router.post("/newUser", async (req, res) => {
+  const {
+    username,
+    password,
+    firstName,
+    lastName,
+    email,
+    createDate,
+    updateDate,
+  } = req.body;
+
+  try {
+    // Check if the username already exists
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+      // User with the provided username already exists
+      res.status(409).send("Username already exists");
+    } else {
+      // Hash the password before saving it
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Create a new user instance
+      const newUser = new User({
+        username,
+        password: hashedPassword,
+        firstName,
+        lastName,
+        email,
+        createDate,
+        updateDate,
+      });
+
+      // Save the new user to the database
+      await newUser.save();
+
+      const returnUser = {
+        userId: newUser._id,
+        username: newUser.username,
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+      };
+
+      // Respond with the new user data
+      res.status(201).send(returnUser);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 // Login route
 router.post("/", async (req, res) => {
   const { username, password } = req.body;
 
   try {
     // Replace 'User' with the actual User model in your application
-    const user = await User.findOne({ username, password });
-    const returnUser = {
-      "userId": user._id,
-      "username": user.username,
-      "firstName": user.firstName,
-      "lastName": user.lastName
-    }
-    if (returnUser) {
+    const user = await User.findOne({ username });
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const returnUser = {
+        userId: user._id,
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+      };
+
       // User with the provided username and password exists
       res.status(200).send(returnUser);
     } else {
